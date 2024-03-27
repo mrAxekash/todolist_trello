1af4bba332f61fcb6d53df5c17049053cc6bba82
mrAxekash <aliaxandr_ku@mail.ru>
2024-03-27 23:04:28 +0300

    lesson 12.3 finish

---
+++ integration/addItemForm.test.js
@@ -0,0 +1,9 @@
+describe('addItemForm', () => {
+    it('base example, visually looks correct', async () => {
+        await page.goto('http://localhost:9009/iframe.html?args=&id=todolist-additemform--add-item-form-story&viewMode=story', {waitUntil: "networkidle2"});
+
+        const image = await page.screenshot();
+
+        expect(image).toMatchImageSnapshot();
+    });
+});
